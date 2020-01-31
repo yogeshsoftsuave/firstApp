3797341c8bcf39339b94e021cbc0acaad706c881
yogeshsoftsuave <yogeshkumar@softsuave.com>
2020-01-31 19:24:08 +0530

    second commit

---
+++ src/main/java/com/security/UserRepository.java
@@ -0,0 +1,9 @@
+package com.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	User findByUsername(String username);
+
+}
